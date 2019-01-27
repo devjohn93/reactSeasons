507812247b764123c3232d087008d0aa5632b8f0
Sagi Janos <jsagi@adverticum.com>
2019-01-27 18:01:42 +0100

    get current user location

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const SeasonDisplay = () => {
+    // get the current user position
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    )
     return <div>Seasons</div>
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import reactDOM from 'react-dom';
 
  const App = () => {
+     // get the current user position
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    )
   return (
     <div>
       Hi there!
